17058178317130e95e21d8f267b6bcec33e471cd
Vadim Shvetsov <vadim.shvetsov@gmail.com>
2016-05-03 11:42:27 +0300

    Generate models for Access and Calendar tables via Gii

---
+++ models/Calendar.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "clndr_calendar".
+ *
+ * @property integer $id
+ * @property string $text
+ * @property integer $creator
+ * @property string $date_event_start
+ * @property string $date_event_end
+ *
+ * @property ClndrUser $creator0
+ */
+class Calendar extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'clndr_calendar';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['text', 'creator'], 'required'],
+            [['text'], 'string'],
+            [['creator'], 'integer'],
+            [['date_event_start', 'date_event_end'], 'safe'],
+            [['creator'], 'exist', 'skipOnError' => true, 'targetClass' => ClndrUser::className(), 'targetAttribute' => ['creator' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'text' => Yii::t('app', 'Text'),
+            'creator' => Yii::t('app', 'Creator'),
+            'date_event_start' => Yii::t('app', 'Date Event Start'),
+            'date_event_end' => Yii::t('app', 'Date Event End'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreator0()
+    {
+        return $this->hasOne(ClndrUser::className(), ['id' => 'creator']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\query\CalendarQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \app\models\query\CalendarQuery(get_called_class());
+    }
+}
